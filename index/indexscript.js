4cf2feecdebee177feb1ae263317281512e6aa7e
Ben Harrison <bph958@uowmail.edu.au>
2018-10-16 15:19:21 +1100

    more work

--- index/indexscript.js
+++ index/indexscript.js
@@ -7,6 +7,9 @@ const Store = require('electron-store');
 const store = new Store();
 const {shell, ipcRenderer} = electron
 
+
+
+
   document.getElementById("min-btn").addEventListener("click", function (e) {   // function to minimize the window
     const window = remote.getCurrentWindow();
     window.minimize(); 
